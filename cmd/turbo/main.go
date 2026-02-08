b304e4b9c9db318feddf5ac4f1c4f8d72e192126
gigamovie <gigafilm21@gmail.com>
2026-02-08 12:33:57 +0700

    Update main.go

--- cmd/turbo/main.go
+++ cmd/turbo/main.go
@@ -1,1 +1,23 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"turbo-downloader/internal/engine"
+)
+
+func main() {
+	url := flag.String("url", "", "Download URL")
+	out := flag.String("o", "file.bin", "Output file")
+	parts := flag.Int("n", 16, "Connections")
+	flag.Parse()
+
+	if *url == "" {
+		log.Fatal("URL wajib diisi")
+	}
+
+	log.Println("Turbo Download started...")
+	engine.Download(*url, *out, *parts)
+	log.Println("Done!")
+}
 
